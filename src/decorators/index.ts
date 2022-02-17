8931c2ad85235cdba68188da5d5f244077eb6856
Simone Di Cicco <simonedicicco@Simones-MacBook-Pro-2.local>
2022-02-17 11:50:17 +0100

    chore: fixing exports

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -4,3 +4,5 @@ export * from './query.decorator';
 export * from './resolver.decorator';
 export * from './parent.decorator';
 export * from './args.decorator';
+export * from './context.decorator';
+export * from './info.decorator';
