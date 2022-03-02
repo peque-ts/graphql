685022f9d968c3bfbb99a1b6eef13f6983beb1f0
Simone Di Cicco <simone.dicicco@gmail.com>
2022-03-02 07:55:29 +0100

    integrations: wip

--- test/http-terminator.ts
+++ test/http-terminator.ts
@@ -1,24 +1,19 @@
 import { Server } from 'http';
+import { Socket } from 'net';
 
-let sockets = {};
-let socketId = 0;
+const sockets = new Set<Socket>();
 
 export function httpTerminator(server: Server): void {
   server.on('connection', (socket) => {
-    socketId++;
-    sockets[socketId] = socket;
-
-    socket.on('close', () => delete sockets[socketId]);
-    socket.setTimeout(0);
+    socket.once('close', () => sockets.delete(socket));
+    sockets.add(socket);
   });
 }
 
 export function killServer(server: Server): void {
-  for (const socket in sockets) {
-    sockets[socket].destroy();
+  for (const socket of sockets) {
+    socket.destroy();
   }
   server.close();
-
-  sockets = {};
-  socketId = 0;
+  sockets.clear();
 }
